af57d4a6086d66793371888021bcd54f9b8ee4cb
Pavithra <vspavipavi@gmail.com>
2023-10-18 20:49:23 +0530

    project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Contact/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Footer/>
+
+
+
+
+
+    </>
+  )
+}
+export default App
